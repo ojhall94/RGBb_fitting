66774c06c7ad87c77b3006129f894b6410b2dec2
Oliver Hall <ojh251@student.bham.ac.uk>
2017-12-19 14:23:58 +0000

    Made some additions to RGBb_mixmod and added a gaussian-noise straight line fit model to cLLModels.

--- cLLModels.py
+++ cLLModels.py
@@ -11,6 +11,9 @@ class LLModels:
         self.locb = np.where(np.array(_labels)=='$b$')[0][0]
         self.locsigb = np.where(np.array(_labels)==r"$\sigma(b)$")[0][0]
         self.loclambd = np.where(np.array(_labels)==r"$\lambda$")[0][0]
+        self.locsigma = np.where(np.array(_labels)==r"$\sigma$")[0][0]
+        self.locm = np.where(np.array(_labels)==r"$m$")[0][0]
+        self.locc = np.where(np.array(_labels)==r"$c$")[0][0]
 
     def gauss_x(self, p):
         '''A simple gaussian in x space'''
@@ -29,3 +32,13 @@ class LLModels:
         A = lambd * (np.exp(lambd*self.x.max()) - np.exp(lambd*self.x.min()))**-1
         lnLx = np.log(A) + lambd*self.x
         return lnLx
+
+    def gauss_line_y(self, p):
+        '''A fit in y for a straight line with gaussian noise.'''
+        sigma = p[self.locsigma]
+        m = p[self.locm]
+        c = p[self.loc]
+
+        M = self.x * m + c
+        lnLy = -0.5 * (((M - self.y) / sigma)**2 + 2*np.log(sigma) + np.log(2*np.pi))
+        return lnLy
